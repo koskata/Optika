6a6e2fa6bfe752bf0c0e86c134e26adb666b94b0
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2026-09-20 15:06:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
    но пази всичко само на устройството.
    ============================================================ */
 window.APP_CONFIG = {
-  supabaseUrl: "",      // напр. "https://abcdefghijkl.supabase.co"
-  supabaseAnonKey: ""   // дългият публичен ключ "anon / publishable"
+  supabaseUrl: "idqmhbiaguuadxaxivzv",      // напр. "https://abcdefghijkl.supabase.co"
+  supabaseAnonKey: "sb_publishable_1FJKYVtWsQ3XOtYmKaerkw_IX0vNrqR"   // дългият публичен ключ "anon / publishable"
 };
