e5fd007f09ae1a4c16f32718673b74674c2ddfc6
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2026-09-22 22:16:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: държи приложението достъпно и без интернет. */
-const CACHE = "optika-v7";
+const CACHE = "optika-v8";
 const SHELL = ["./", "./index.html", "./config.js", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"];
 
